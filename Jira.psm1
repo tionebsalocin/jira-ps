ef17ce01cec1234fea2a73cb964bc15b5893b60d
Patrick Goncalves <p.goncalves@criteo.com>
2015-11-27 08:36:03 +0100

    code refacto by the Colonel

--- Jira.psm1
+++ Jira.psm1
@@ -1,139 +1,141 @@
 ﻿Function ConvertTo-SafeUri($uri) {
-    Return [System.Uri]::EscapeDataString($uri)
+  Return [System.Uri]::EscapeDataString($uri)
 }
 
 Function Set-JiraApiBase {
-    Param (
-        [Parameter (Mandatory=$True)]
-        [string] $jira_api_base
-    )
-        
-        # This will take the format http://jira.domain.com/rest/api/2/
-        $env:JIRA_API_BASE = $jira_api_base
-        Write-Host "Jira Api Base Set:"
-        Write-Host $env:JIRA_API_BASE
+Param (
+  [Parameter (Mandatory=$True)]
+  [string] $jira_api_base
+)
+      
+  # This will take the format http://jira.domain.com/rest/api/2/
+  $env:JIRA_API_BASE = $jira_api_base
+  Write-Host "Jira Api Base Set:"
+  Write-Host $env:JIRA_API_BASE
 }
 
 
 Function Set-JiraHttpProxy {
-    Param (
-        [Parameter (Mandatory=$True)]
-        [string] $Httpproxy
-    )
-	$env:JIRA_HTTP_PROXY = $Httpproxy
-    Write-Host "Jira proxy Set:"
-    Write-Host $env:JIRA_HTTP_PROXY
+Param (
+  [Parameter (Mandatory=$True)]
+  [string] $Httpproxy
+)
+  $env:JIRA_HTTP_PROXY = $Httpproxy
+  Write-Host "Jira proxy Set:"
+  Write-Host $env:JIRA_HTTP_PROXY
 }
 
 
 Function Set-JiraCredentials {
-    Param(
-        [Parameter(Mandatory=$True, Position=1)]
-        [string]$username,
+Param(
+  [Parameter(Mandatory=$True, Position=1)]
+  [string]$username,
 
-        [Parameter(Mandatory=$True, Position=2)]
-        [System.Security.SecureString]$password
-    )
+  [Parameter(Mandatory=$True, Position=2)]
+  [System.Security.SecureString]$password
+)
 
-    $env:JIRA_CREDENTIALS = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("${username}:$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)))"))
+  $env:JIRA_CREDENTIALS = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("${username}:$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)))"))
 }
 
-Function Invoke-JiraRequest($method, $request, $body) {
-    If ($env:JIRA_API_BASE -eq $Null) {
-        Write-Error "JIRA API Base has not been set, please run ``Set-JiraApiBase'"
-    }
-    If ($env:JIRA_CREDENTIALS -eq $Null) {
-        Write-Error "No JIRA credentials have been set, please run ``Set-JiraCredentials'"
-    }
-    Write-Debug "Calling $method $env:JIRA_API_BASE$request with AUTH: Basic $env:JIRA_CREDENTIALS $env:JIRA_HTTP_PROXY"
-    If ($body -eq $Null) {
-        Return Invoke-RestMethod -Uri "${env:JIRA_API_BASE}${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method -ContentType "application/json" -Proxy $env:JIRA_HTTP_PROXY
-    }
-    else {
-        Return Invoke-RestMethod -Uri "${env:JIRA_API_BASE}${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method -Body $body -ContentType "application/json" -Proxy $env:JIRA_HTTP_PROXY
-    }
+Function Invoke-JiraRequest($method, $request, $body, $userAgent='Jira.psm1') {
+  If ($env:JIRA_API_BASE -eq $Null) {
+      Write-Error "JIRA API Base has not been set, please run ``Set-JiraApiBase'"
+  }
+  If ($env:JIRA_CREDENTIALS -eq $Null) {
+      Write-Error "No JIRA credentials have been set, please run ``Set-JiraCredentials'"
+  }
+  Write-Debug "Calling $method $env:JIRA_API_BASE$request with AUTH: Basic $env:JIRA_CREDENTIALS $env:JIRA_HTTP_PROXY"
+  If ($body -eq $Null) {
+      Return Invoke-RestMethod -Uri "${env:JIRA_API_BASE}${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method -ContentType "application/json" -UserAgent $userAgent
+  }
+  else {
+      Return Invoke-RestMethod -Uri "${env:JIRA_API_BASE}${request}" -Headers @{"AUTHORIZATION"="Basic $env:JIRA_CREDENTIALS"} -Method $method -Body $body -ContentType "application/json" -UserAgent $userAgent
+  }
 }
 
 # Begin Get Functions
 Function Get-JiraGroup($group) {
-    Return Invoke-JiraRequest GET "group?groupname=$(ConvertTo-SafeUri $group)&expand"
+  Return Invoke-JiraRequest GET "group?groupname=$(ConvertTo-SafeUri $group)&expand"
 }
 
 Function Get-JiraHistory($issue) {
-    Return Invoke-JiraRequest GET "issue/$(ConvertTo-SafeUri $issue)?expand=changelog"
+  Return Invoke-JiraRequest GET "issue/$(ConvertTo-SafeUri $issue)?expand=changelog"
 }
 
 Function Get-JiraIssue($issue) {
-    Return Invoke-JiraRequest GET "issue/$(ConvertTo-SafeUri $issue)"
+  Return Invoke-JiraRequest GET "issue/$(ConvertTo-SafeUri $issue)"
 }
 
 
 Function Get-JiraSearchResult($query) {
-    Return Invoke-JiraRequest GET "search?jql=$(ConvertTo-SafeUri $query)"
+  Return Invoke-JiraRequest GET "search?jql=$(ConvertTo-SafeUri $query)"
 }
 
 Function Get-JiraProjectList {
-    # Returns All Projects on a Jira Server
-    Return Invoke-JiraRequest GET "project"
+  # Returns All Projects on a Jira Server
+  Return Invoke-JiraRequest GET "project"
 }
 
 Function Get-JiraProject($project) {
-    # Returns a Particular Project
-    Return Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $project)"
+  # Returns a Particular Project
+  Return Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $project)"
 }
 
 Function Get-JiraProjectRole($project, $role) {
-    # Returns Users and Groups in a Role in a Jira Project (10000 - Users; 10002 - Administrators; 10001 - Developers)
-    Return Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $project)/role/$(ConvertTo-SafeUri $role)"
+  # Returns Users and Groups in a Role in a Jira Project (10000 - Users; 10002 - Administrators; 10001 - Developers)
+  Return Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $project)/role/$(ConvertTo-SafeUri $role)"
 }
 # End Get Functions
 
 # Begin Start Functions
 Function Start-JiraBackgroundReIndex {
-    Return Invoke-JiraRequest POST "reindex"
+  Return Invoke-JiraRequest POST "reindex"
 }
 # End Start Functions
 
 # Begin Add Functions
 Function Add-JiraGrouptoProject($project, $role, $json) {
-    # $json should be valid json like: 
-    # { "user" : ["admin"] }  
-    # or
-    # { "group" : ["jira-developers"] }
-    Return Invoke-JiraRequest POST "project/$(ConvertTo-SafeUri $project)/role/$(ConvertTo-SafeUri $role)" $json
+  # $json should be valid json like: 
+  # { "user" : ["admin"] }  
+  # or
+  # { "group" : ["jira-developers"] }
+  Return Invoke-JiraRequest POST "project/$(ConvertTo-SafeUri $project)/role/$(ConvertTo-SafeUri $role)" $json
 }
 
 
 Function add-JiraIssue {
-   param (
-   [Parameter(Mandatory = $true,Position = 0)]
-   [string] $Projectname,
-   [Parameter(Mandatory = $true,Position = 1)]
-   [string] $Summary,
-   [Parameter(Mandatory = $true,Position = 2)]
-   [string] $Description,
-   [Parameter(Mandatory = $true,Position = 3)]
-   [string] $issuetype,
-   [Parameter(Mandatory = $true,Position = 4)]
-   [string] $assignee
-   )
+param (
+  [Parameter(Mandatory = $true,Position = 0)]
+  [string] $Projectname,
+  [Parameter(Mandatory = $true,Position = 1)]
+  [string] $Summary,
+  [Parameter(Mandatory = $true,Position = 2)]
+  [string] $Description,
+  [Parameter(Mandatory = $true,Position = 3)]
+  [string] $issuetype,
+  [Parameter(Mandatory = $true,Position = 4)]
+  [string] $assignee,
+  [Parameter(Mandatory = $true,Position = 5)]
+  [string] $userAgent='Jira.psm1'
+)
 
-   $res=Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $projectname)"
-   $projectid=$res.key
+  $res=Invoke-JiraRequest GET "project/$(ConvertTo-SafeUri $projectname)"
+  $projectid=$res.key
 
-   $fields = New-Object -TypeName PSObject -Property ([ordered]@{
-   "project"=@{key ="$projectid";}
-   "summary"=$Summary;
-   "description"=$Description;
-   "issuetype"=@{name=$issuetype;}
-   "assignee"=@{name=$assignee;}
-   })
+  $fields = New-Object -TypeName PSObject -Property ([ordered]@{
+  "project"=@{key ="$projectid";}
+  "summary"=$Summary;
+  "description"=$Description;
+  "issuetype"=@{name=$issuetype;}
+  "assignee"=@{name=$assignee;}
+  })
 
-   $json = New-Object -TypeName PSObject -Property (@{"fields"=$fields}) | ConvertTo-Json
-   Write-verbose "Created JSON object:`n$json"
+  $json = New-Object -TypeName PSObject -Property (@{"fields"=$fields}) | ConvertTo-Json
+  Write-verbose "Created JSON object:`n$json"
 
-   $jiraissue=Invoke-JiraRequest POST "issue" $json
-   "$jiraissue.key Created"
+  $jiraissue=Invoke-JiraRequest POST "issue" $json $userAgent
+  "$jiraissue.key Created"
 
 }
 
